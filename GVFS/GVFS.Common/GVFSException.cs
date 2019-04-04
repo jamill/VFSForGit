e75a04e493a614d1a34590ef9f563f5e8bd422a3
Jameson Miller <jamill@microsoft.com>
2019-04-04 13:24:00 -0400

    Introduce GVFSException
    This exception will be used by GVFS to indicate an error was encountered.

---
+++ GVFS/GVFS.Common/GVFSException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GVFS.Common
+{
+    public class GVFSException : System.Exception
+    {
+        /// <summary>
+        /// Base exception to indicate a GVFS error
+        /// </summary>
+        public GVFSException()
+            : base()
+        {
+        }
+
+        public GVFSException(string message)
+            : base(message)
+        {
+        }
+    }
+}

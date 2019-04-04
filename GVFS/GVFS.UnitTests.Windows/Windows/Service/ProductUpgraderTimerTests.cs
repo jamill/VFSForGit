c113465e1dfcd7ba878043cae6e14ce19f5d7782
Jameson Miller <jamill@microsoft.com>
2019-04-04 14:29:20 -0400

    ProductUpgradeTimer: add test harness

---
+++ GVFS/GVFS.UnitTests.Windows/Windows/Service/ProductUpgraderTimerTests.cs
@@ -0,0 +1,30 @@
+﻿using GVFS.Common.FileSystem;
+using GVFS.Service;
+using GVFS.UnitTests.Mock.Common;
+using GVFS.UnitTests.Windows.Mock.Upgrader;
+using Moq;
+using NUnit.Framework;
+
+namespace GVFS.UnitTests.Windows.Windows.Service
+{
+    public class ProductUpgraderTimerTests
+    {
+        [TestCase]
+        public void QueryGitHubForUpdates()
+        {
+            MockTracer tracer = new MockTracer();
+            Mock<PhysicalFileSystem> fileSystemMock = new Mock<PhysicalFileSystem>();
+            MockLocalGVFSConfig gvfsConfig = new MockLocalGVFSConfig();
+
+            using (ProductUpgradeTimer upgradeChecker = new ProductUpgradeTimer(tracer, fileSystemMock.Object, gvfsConfig))
+            {
+                upgradeChecker.Start();
+            }
+        }
+
+        [TestCase]
+        public void QueriesOrgInfoServerForUpdates()
+        {
+        }
+    }
+}
